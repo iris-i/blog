d02f0f68e746ef1fe805e21ad72c2b4053b02cfa
Iris Ibekwe <iris.ibekwe@civicactions.com>
2018-08-31 16:38:12 -0400

    Set typography and font sizes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import './_footer.scss';
 
 const Footer = (props) => (
   <section className = "footer">
     <footer role = "contentinfo" className = "welcome">
-      Proudly powered by <a href = "www.gatsbyjs.org">Gatsby</a>
+      Proudly powered by <a href = "http://gatsbyjs.org">Gatsby</a>
     </footer>
   </section>
 );
